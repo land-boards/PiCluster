8385457eb40bcebb2f6cba5a86cf45b861b4fa0a
Doug Gilliland <doug@douglasgilliland.com>
2016-07-04 13:32:36 -0400

    calculate an integral

--- ExampleCode/trapParallel_2.py
+++ ExampleCode/trapParallel_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
-#trapParallel_1.py
-#example to run: mpiexec -n 4 python trapParallel_1.py 0.0 1.0 10000
-#http://materials.jeremybejarano.com/MPIwithPython/pointToPoint.html#parallelizing-the-trapezoidal-rule
-
+#trapParallel_2.py
+#example to run: mpiexec -n 4 python26 trapParallel_2.py 0.0 1.0 10000
 import numpy
 import sys
 from mpi4py import MPI
@@ -45,24 +43,17 @@ local_b = local_a + local_n*h
 
 #initializing variables. mpi4py requires that we pass numpy objects.
 integral = numpy.zeros(1)
-recv_buffer = numpy.zeros(1)
+total = numpy.zeros(1)
 
 # perform local computation. Each process integrates its own interval
 integral[0] = integrateRange(local_a, local_b, local_n)
 
 # communication
-# root node receives results from all processes and sums them
-if rank == 0:
-	total = integral[0]
-	for i in range(1, size):
-		comm.Recv(recv_buffer, ANY_SOURCE)
-		total += recv_buffer[0]
-else:
-	# all other process send their result
-	comm.Send(integral)
+# root node receives results with a collective "reduce"
+comm.Reduce(integral, total, op=MPI.SUM, root=0)
 
 # root process prints results
 if comm.rank == 0:
 	print "With n =", n, "trapezoids, our estimate of the integral from"\
 	, a, "to", b, "is", total
-		
+	
