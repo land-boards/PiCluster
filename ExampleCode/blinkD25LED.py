d31cbc5bc9f5d6382d59a95a8cbba169eb910399
Doug Gilliland <doug@douglasgilliland.com>
2016-07-11 17:30:12 -0400

    Blink D25 LED

---
+++ ExampleCode/blinkD25LED.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+'''blinkD25LED.py
+
+Code to blink D25 LED on the RPI-PWR card.
+
+This code must be run as Superuser on the Raspberry Pi.
+
+============
+Dependencies
+============
+
+Need to: 
+
+* sudo apt-get install python-dev
+* sudo apt-get install python-pip python2.7-dev
+* sudo apt-get install python-rpi.gpio
+* sudo pip install flask
+
+====
+Code
+====
+'''
+
+import RPi.GPIO as GPIO
+import os
+import time
+
+# Assign all of the GPIO lines (by board pin numbering) to their corresponding jacks 
+# on the GVS card.
+
+
+def blinkLED(channel):
+	'''Function to blink an LED attached to an output channel
+	Drives line high for a short time and then drives it low.
+	The high level output turns on the LED.
+	'''
+	GPIO.output(channel, 1)
+	time.sleep(0.25)
+	GPIO.output(channel, 0)
+	
+GPIO.setmode(GPIO.BCM)	# setup GPIO using Board numbering
+
+# Set all of the pins to outputs
+GPIO.setup(J25, GPIO.OUT)
+
+# Blink all of the LEDs one at a time forever
+# CTRL-C to exit which is not a particularly elegant exit strategy, but this is a demo program
+
+while 1:
+	blinkLED(25)
